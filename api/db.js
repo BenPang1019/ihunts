538a76e7ff46e19f66f8cf2ceea80a5ef767e6f9
BenPang1019 <benpang1019@gmail.com>
2022-10-16 00:51:01 +0800

    Try

--- api/db.js
+++ api/db.js
@@ -1,8 +1,15 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "test",
+    // host: "127.0.0.1",
+    // user: "root",
+    // password: "",
+    // database: "test",
+
+
+    host: "auth-db343.hstgr.io",
+    user: "u811623433_ihunts",
+    password: "Ihunts123",
+    database: "u811623433_ihunts",
+
 })
